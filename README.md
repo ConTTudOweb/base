0169c5af1f308cb4292ffaaf4ef09edf3b7ddcb2
Alessandro de Lima Folk <alessandro@kminformatica.com.br>
2017-12-23 18:18:08 -0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Base Project
+
+Base project for the main system.
+
+[![Build Status](https://travis-ci.org/ConTTudOweb/base.svg?branch=master)](https://travis-ci.org/ConTTudOweb/base)
+[![Code Health](https://landscape.io/github/ConTTudOweb/base/master/landscape.svg?style=flat)](https://landscape.io/github/ConTTudOweb/base/master)
+[![Coverage Status](https://coveralls.io/repos/github/ConTTudOweb/base/badge.svg?branch=master)](https://coveralls.io/github/ConTTudOweb/base?branch=master)
+
+## How to develop?
+
+1. Clone the repository;
+2. Create a "virtualenv" with Python 3.6;
+3. Activate the "virtualenv";
+4. Upgrade the "pip";
+5. Install the dependencies;
+6. Configure the instance with the ".env";
+7. Run the tests;
+
+```console
+git clone git@github.com:ConTTudOweb/base.git
+cd base
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements-dev.txt
+cp contrib/env-sample .env
+python manage.py test
+```
---
+++ base/authentication/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser,
+    PermissionsMixin)
+from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField(null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
